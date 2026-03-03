386afd7a29e434d2e479e307932b5bd5fbb13530
Charles Chang <charleschang7@yahoo.com>
2026-03-02 19:28:17 -0500

    WORK IN PROGRESS: Adding some initial code to call the leetcode api

--- byte_bot/cogs/leetcode.py
+++ byte_bot/cogs/leetcode.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+#import requests
+import sys
 
 class leetcode(commands.Cog):
     def __init__(self, bot):
@@ -12,7 +14,39 @@ class leetcode(commands.Cog):
     @commands.hybrid_command()
     async def leetcode(self, ctx):
         ### TODO add all the code to take in leetcode params and call a leetcode api
+        print('sys exec =' + sys.executable)
         await ctx.send("Hello from the leetcode function")
+        # url = "https://leetcode.com/graphql"
+        # query = """
+        #         query getUserProfile($username: String!) {
+        #         matchedUser(username: $username) {
+        #             username
+        #             profile {
+        #             realName
+        #             ranking
+        #             reputation
+        #             countryName
+        #             }
+        #             submitStats: submitStatsGlobal {
+        #             acSubmissionNum {
+        #                 difficulty
+        #                 count
+        #             }
+        #             }
+        #         }
+        #         }
+        #         """
+        # variables = {
+        #     "username": "charleschang7"   # replace with target username
+        # }
+        # response = requests.post(
+        #     url,
+        #     json={"query": query, "variables": variables}
+        # )
+
+        # data = response.json()
+        # print(data)
+
     
 async def setup(bot):
     await bot.add_cog(leetcode(bot))

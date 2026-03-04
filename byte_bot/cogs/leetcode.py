470716ebbfa2aaef6d9a8eca90f2808b654339da
Charles Chang <charleschang7@yahoo.com>
2026-03-04 15:49:20 -0500

    Added discord embed to display profile and added dataclasses for easy field access

--- byte_bot/cogs/leetcode.py
+++ byte_bot/cogs/leetcode.py
@@ -2,6 +2,27 @@ import discord
 from discord.ext import commands
 import requests
 import sys
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class SubmissionStat:
+    difficulty: str
+    count: int
+
+@dataclass
+class Profile:
+    real_name: str
+    ranking: int
+    reputation: int
+    country: str
+
+@dataclass
+class LeetCodeUser:
+    username: str
+    profile: Profile
+    submissions: List[SubmissionStat]
 
 class leetcode(commands.Cog):
     def __init__(self, bot):
@@ -11,14 +32,16 @@ class leetcode(commands.Cog):
     async def on_ready(self):
         print("Bytebot is online")
     
-    @commands.hybrid_command()
-    async def leetcode(self, ctx, profile: str):
 
-        if not profile:
-            await ctx.send("Please enter the username of the leetcode profile you want to query. Ex: +leetcode <username>")
+    @commands.hybrid_command()
+    async def leetcode(self, ctx, profile: str = None):
+
+        if profile is None:
+            await ctx.send("You must provide a leetcode username.\nUsage: `+leetcode <username>`")
             return
 
         url = "https://leetcode.com/graphql"
+
         # This is where the graphql query happens, we can also query for problems, found this documentation https://jacoblincool.github.io/LeetCode-Query/
         query = """
                 query getUserProfile($username: String!) {
@@ -39,6 +62,7 @@ class leetcode(commands.Cog):
                 }
                 }
                 """
+        
         variables = {
             "username": f"{profile}"
         }
@@ -48,7 +72,39 @@ class leetcode(commands.Cog):
         )
 
         data = response.json()
-        await ctx.send(data)
+
+        # Parse the data
+        user_data = data["data"]["matchedUser"]
+
+        profile_data = user_data["profile"]
+        submission_data = user_data["submitStats"]["acSubmissionNum"]
+
+        profile = Profile(
+            real_name=profile_data["realName"],
+            ranking=profile_data["ranking"],
+            reputation=profile_data["reputation"],
+            country=profile_data["countryName"],
+        )
+
+        submissions = [
+            SubmissionStat(
+                difficulty=stat["difficulty"],
+                count=stat["count"]
+            )
+            for stat in submission_data
+        ]
+
+        user = LeetCodeUser(
+            username=user_data["username"],
+            profile=profile,
+            submissions=submissions
+        )
+        
+        # Create a discord Embed object to display
+        embed = discord.Embed()
+        embed.add_field(name="User", value=f"{user}")
+
+        await ctx.send(embed=embed)
 
     
 async def setup(bot):

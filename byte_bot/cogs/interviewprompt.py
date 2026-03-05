1bbc4baa6d478b141024caa95788491cc70b4e0c
Sumika <shelffr@wgu.edu>
2026-03-05 16:06:44 +0900

    Add sample questions as a list

--- byte_bot/cogs/interviewprompt.py
+++ byte_bot/cogs/interviewprompt.py
@@ -1,22 +1,35 @@
 import discord
 from discord.ext import commands
+import random
 
 class InterviewPrompt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        # Sample question bank
+        # Source: https://www.geeksforgeeks.org/python/python-interview-questions/
+        self.questions = [
+            "Is Python a compiled language or an interpreted language?",
+            "How can you concatenate two lists in Python?",
+            "Difference between for loop and while loop in Python",
+            "How do you floor a number in Python?",
+            "What is the difference between / and // in Python?"
+        ]
+
     @commands.Cog.listener()
     async def on_ready(self):
         print('InterviewPrompt cog is ready!')
 
+# Work in progress
     @commands.hybrid_command()
     async def interviewprompt(self, ctx):
+        question = random.choice(self.questions)
 
-        # discord Embed
-        embed = discord.Embed(title="embed test", description="embed testing", color=discord.Color.blue())
-        embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/1.png")
-        embed.add_field(name="Interview prompt", value="test question")
-        embed.set_footer(text="Byte Club")
+        # Discord embed
+        embed = discord.Embed(title="Ready for an interview?", description="Try answering these Python questions!", color=discord.Color.blue())
+        embed.add_field(name="Question:", value=question, inline=False)
+        embed.add_field(name="Answer:", value="[Check answer](https://www.geeksforgeeks.org/python/python-interview-questions/)", inline=False)
+        embed.set_footer(text="🤖Byte Club😸")
 
         await ctx.send(embed=embed, ephemeral=True)
 

23671d4789ffaad1fca9440bce1a332b00647ee1
Natalie Valcin <twin_1118@icloud.com>
2026-03-07 14:17:11 -0500

    create config file and load dotenv in config before env vars are read

--- byte_bot/__main__.py
+++ byte_bot/__main__.py
@@ -1,31 +1,23 @@
 import logging
-from os import environ
-
-from dotenv import load_dotenv
 
 from byte_bot.byte_bot import ByteBot
+from byte_bot.config import Config
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
 def main():
-    required_env_vars = [
-        "DISCORD_TOKEN",
-        "FEATURE_FORUM_CHANNEL_ID"
-    ]
-    if missing_tokens:= [var for var in required_env_vars if not environ.get(var)]: # Ensure the token is set before proceeding
-        raise ValueError(f"Missing environment variables: {', '.join(missing_tokens)}")
-    bot = ByteBot(
-        feature_forum_channel_id=int(environ["FEATURE_FORUM_CHANNEL_ID"])
-    )
-    bot.run(environ["DISCORD_TOKEN"])
+    if not (token := Config.DISCORD_TOKEN):  # Ensure the token is set before proceeding
+        raise ValueError("DISCORD_TOKEN environment variable is not set.")
+    if not Config.FEATURE_FORUM_CHANNEL_ID:
+        raise ValueError("FEATURE_FORUM_CHANNEL_ID environment variable is not set.")
+    bot = ByteBot(feature_forum_channel_id=Config.FEATURE_FORUM_CHANNEL_ID)
+    bot.run(token)
 
 
 # Entry point for the application
 if __name__ == "__main__":
-    load_dotenv()
-
     try:
         log.debug("Starting byte-bot...")
         main()

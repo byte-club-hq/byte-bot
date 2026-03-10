8628cb47bd7b43b23779c59137b70b10388312ff
Charles Chang <charleschang7@yahoo.com>
2026-03-10 16:48:53 -0400

    Added a random question generator with a difficulty filter

--- byte_bot/cogs/leetcode.py
+++ byte_bot/cogs/leetcode.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 import requests
-import sys
+import re
+import random
 from dataclasses import dataclass
 from typing import List
 
@@ -25,6 +26,7 @@ class LeetCodeUser:
     submissions: List[SubmissionStat] | None
 
 class leetcode(commands.Cog):
+    url = "https://leetcode.com/graphql"
     def __init__(self, bot):
         self.bot = bot
 
@@ -34,98 +36,92 @@ class leetcode(commands.Cog):
     
 
     @commands.hybrid_command()
-    async def leetcode(self, ctx, command: str = None):
+    async def leetcodeprofile(self, ctx, profile: str = None):
 
-        if command is None:
-            await ctx.send("You must provide a leetcode username.\nUsage: `/leetcode profile <username>`\n\nAlternatively provide daily to obtain the daily leetcode problem\nUsage: `/leetcode daily`", ephemeral=True)
+        if profile is None:
+            await ctx.send("You must provide a leetcode username.\nUsage: `/leetcodeprofile <username>`\n", ephemeral=True)
             return
 
-        url = "https://leetcode.com/graphql"
-
-        input = command.split()
-
-        query = ""
-        
         # This is where the graphql query happens, we can also query for problems, found this documentation https://jacoblincool.github.io/LeetCode-Query/
-        match input[0]:
-            case 'profile':
-                query = """
-                query getUserProfile($username: String!) {
-                matchedUser(username: $username) {
-                    username
-                    profile {
-                    realName
-                    ranking
-                    reputation
-                    countryName
-                    }
-                    submitStats: submitStatsGlobal {
-                    acSubmissionNum {
-                        difficulty
-                        count
-                    }
-                    }
-                }
-                }
-                """
-                variables = {
-                    "username": f"{input[1]}"
-                }
-                response = requests.post(
-                    url,
-                    json={"query": query, "variables": variables}
-                )
+        query = """
+        query getUserProfile($username: String!) {
+        matchedUser(username: $username) {
+            username
+            profile {
+            realName
+            ranking
+            reputation
+            countryName
+            }
+            submitStats: submitStatsGlobal {
+            acSubmissionNum {
+                difficulty
+                count
+            }
+            }
+        }
+        }
+        """
+        variables = {
+            "username": f"{profile}"
+        }
+        response = requests.post(
+            self.url,
+            json={"query": query, "variables": variables}
+        )
 
-                data = response.json()
+        data = response.json()
 
-                # First check if that users profile exists
-                if not (user_data := data.get("data").get("matchedUser")):
-                    await ctx.send("Failed to find a leetcode user with that username", ephemeral=True)
-                    return
+        # First check if that users profile exists
+        if not (user_data := data.get("data").get("matchedUser")):
+            await ctx.send("Failed to find a leetcode user with that username", ephemeral=True)
+            return
 
-                # Parse the data
-                profile_data = user_data.get("profile")
-                submission_data = user_data.get("submitStats").get("acSubmissionNum")
+        # Parse the data
+        profile_data = user_data.get("profile")
+        submission_data = user_data.get("submitStats").get("acSubmissionNum")
 
-                profile = Profile(
-                    real_name=profile_data.get("realName"),
-                    ranking=profile_data.get("ranking"),
-                    reputation=profile_data.get("reputation"),
-                    country=profile_data.get("countryName"),
-                )
+        profile = Profile(
+            real_name=profile_data.get("realName"),
+            ranking=profile_data.get("ranking"),
+            reputation=profile_data.get("reputation"),
+            country=profile_data.get("countryName"),
+        )
 
-                submissions = [
-                    SubmissionStat(
-                        difficulty=stat.get("difficulty"),
-                        count=stat.get("count")
-                    )
-                    for stat in submission_data
-                ]
+        submissions = [
+            SubmissionStat(
+                difficulty=stat.get("difficulty"),
+                count=stat.get("count")
+            )
+            for stat in submission_data
+        ]
 
-                user = LeetCodeUser(
-                    username=user_data.get("username"),
-                    profile=profile,
-                    submissions=submissions
-                )
-                
-                # Create a discord Embed object to display
-                embed = discord.Embed()
+        user = LeetCodeUser(
+            username=user_data.get("username"),
+            profile=profile,
+            submissions=submissions
+        )
+        
+        # Create a discord Embed object to display
+        embed = discord.Embed()
 
-                embed.add_field(name="User", value=user.username, inline=False)
+        embed.add_field(name="User", value=user.username, inline=False)
 
-                embed.add_field(name="Profile", value=user.profile.real_name, inline=False)
-                embed.add_field(name="Ranking", value=user.profile.ranking, inline=True)
-                embed.add_field(name="Reputation", value=user.profile.reputation, inline=True)
+        embed.add_field(name="Profile", value=user.profile.real_name, inline=False)
+        embed.add_field(name="Ranking", value=user.profile.ranking, inline=True)
+        embed.add_field(name="Reputation", value=user.profile.reputation, inline=True)
 
-                embed.add_field(name="Submissions", value="Total Problems solved 🎉", inline=False)
+        embed.add_field(name="Submissions", value="Total Problems solved 🎉", inline=False)
 
-                for stat in submissions:
-                    embed.add_field(name=f"{stat.difficulty}", value=f"{stat.count}", inline=True)
+        for stat in submissions:
+            embed.add_field(name=f"{stat.difficulty}", value=f"{stat.count}", inline=True)
 
-                await ctx.send(embed=embed, ephemeral=True)
+        await ctx.send(embed=embed, ephemeral=True)
 
-            case 'daily':
-                query = """
+
+    @commands.hybrid_command()
+    async def leetcodedaily(self, ctx):
+        query = """
                 query questionOfToday {
                 activeDailyCodingChallengeQuestion {
                     date
@@ -140,34 +136,68 @@ class leetcode(commands.Cog):
                 }
                 }
                 """
-                variables = {
-                }
-                response = requests.post(
-                    url,
-                    json={"query": query, "variables": variables}
-                )
 
-                data = response.json()
-                # Parse the daily challenge json
-                if not (problem_data := data.get("data").get("activeDailyCodingChallengeQuestion")):
-                    await ctx.send("Failed to find a daily leetcode problem", ephemeral=True)
-                    return
+        variables = {
+        }
+        response = requests.post(
+            self.url,
+            json={"query": query, "variables": variables}
+        )
 
-                print(problem_data.get("question").get("title"))
-                print("https://leetcode.com" + (problem_data.get("link")))
+        data = response.json()
+        # Parse the daily challenge json
+        if not (problem_data := data.get("data").get("activeDailyCodingChallengeQuestion")):
+            await ctx.send("Failed to find a daily leetcode problem", ephemeral=True)
+            return
 
-                embed = discord.Embed()
-                embed.add_field(name="Title", value=problem_data.get("question").get("title"), inline=False)
-                embed.add_field(name="Difficulty", value=problem_data.get("question").get("difficulty"), inline=False)
-                embed.add_field(name="Link", value="https://leetcode.com" + (problem_data.get("link")), inline=False)
-                await ctx.send(embed=embed, ephemeral=True)
-            case _:
-                print("Unknown command")
+        embed = discord.Embed()
+        embed.add_field(name="Title", value=problem_data.get("question").get("title"), inline=False)
+        embed.add_field(name="Difficulty", value=problem_data.get("question").get("difficulty"), inline=False)
+        embed.add_field(name="Link", value="https://leetcode.com" + (problem_data.get("link")), inline=False)
+        await ctx.send(embed=embed, ephemeral=True)
+
+
+    @commands.hybrid_command(
+        name="leetcoderandom",
+        description="Get a random LeetCode problem by difficulty"
+        )
+    async def leetcoderandom(self, ctx, difficulty: str = None):
+        # Because filtering is no longer supported in the graphql endpoint I am querying the rest endpoint for all problems
+        url = "https://leetcode.com/api/problems/all/"
+
+        if difficulty is None:
+            await ctx.send("You must provide a difficulty.\nUsage: `/leetcoderandom <difficulty>`\n", ephemeral=True)
+            return
         
-                
+        # Strip all the spaces and newlines from the input and convert it to all uppercase
+        difficulty = re.sub(r"[\n\t\s]*", "", difficulty.lower().capitalize())
+                            
+        if(difficulty != "Easy" and difficulty != "Medium" and difficulty != "Hard"):
+            await ctx.send("Please enter easy, medium, or hard for the difficulty.\nUsage: `/leetcoderandom <difficulty>`\n", ephemeral=True)
+            return
+
+        response = requests.get(url)
+        data = response.json()
+        questions = data.get("stat_status_pairs")  # list of all problems
+
+        #print(f"{data}")
+
+        # Filter by difficulty
+        difficulty = f"{difficulty}"
+        filtered = [
+            q for q in questions
+            if q.get("difficulty").get("level") == {"Easy": 1, "Medium": 2, "Hard": 3}.get(difficulty)
+        ]
+        
+        random_problem = random.choice(filtered)
+
+        embed = discord.Embed()
+        embed.add_field(name="Title", value=random_problem.get("stat").get("question__title"), inline=False)
+        embed.add_field(name="Difficulty", value=f"{difficulty}", inline=False)
+        embed.add_field(name="Link", value="https://leetcode.com/problems/" + (random_problem.get("stat").get("question__title_slug")), inline=False)
+        await ctx.send(embed=embed, ephemeral=True)
 
 
 
-    
 async def setup(bot):
     await bot.add_cog(leetcode(bot))

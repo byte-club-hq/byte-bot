ddea78f20b7d79976dad0a8f6c6a1f9ec6be111d
Sumika <shelffr@wgu.edu>
2026-03-13 22:03:13 +0900

    Change question structure to use list of dictionaries

--- byte_bot/cogs/interviewprompt.py
+++ byte_bot/cogs/interviewprompt.py
@@ -8,13 +8,27 @@ class InterviewPrompt(commands.Cog):
 
         # Sample question bank
         # Source: https://www.geeksforgeeks.org/python/python-interview-questions/
-        # Naive approach??
         self.questions = [
-            "Is Python a compiled language or an interpreted language?",
-            "How can you concatenate two lists in Python?",
-            "Difference between for loop and while loop in Python",
-            "How do you floor a number in Python?",
-            "What is the difference between / and // in Python?"
+            {
+                "question": "How can you concatenate two lists in Python?",
+                "answer": "By using the + operator or the extend() method"
+             },
+            {
+                "question": "What is the difference between for loop and while loop in Python",
+                "answer": "For loops are used when we know how many times to repeat, often with lists, tuples, sets, or dictionaries. While loops are used when we only have an end condition and don’t know exactly how many times it will repeat."
+            },
+            {
+                "question": "How do you floor a number in Python?",
+                "answer": "To floor a number in Python, we can use the math.floor() function"
+            },
+            {
+                "question": "What is the difference between / and // in Python?",
+                "answer": "/ represents precise division (result is a floating point number) whereas // represents floor division (result is an integer)"
+            },
+            {
+                "question": "Is Indentation Required in Python?",
+                "answer": "Yes, indentation is required in Python"
+            }
         ]
 
     @commands.Cog.listener()
@@ -24,13 +38,17 @@ class InterviewPrompt(commands.Cog):
 # Work in progress
     @commands.hybrid_command()
     async def interviewprompt(self, ctx):
-        question = random.choice(self.questions)
+        q = random.choice(self.questions)
 
         # Discord embed
-        embed = discord.Embed(title="Ready for an interview?", description="Try answering these Python questions!", color=discord.Color.blue())
-        embed.add_field(name="Question:", value=question, inline=False)
-        # Also naive, you'd have to scroll through the page to find the answer
-        embed.add_field(name="Answer:", value="[Check answer](https://www.geeksforgeeks.org/python/python-interview-questions/)", inline=False)
+        embed = discord.Embed(
+            title="Ready for an interview?",
+            description="Pop Python Quiz! (PPQ for short)",
+            color=discord.Color.blue()
+        )
+        embed.add_field(name="Question:", value=q["question"], inline=False)
+        embed.add_field(name="Answer", value=f"||{q['answer']}||", inline=False)
+        embed.add_field(name="Quiz Source:", value="[link](https://www.geeksforgeeks.org/python/python-interview-questions/)", inline=False)
         embed.set_footer(text="🤖Byte Club😸")
 
         await ctx.send(embed=embed, ephemeral=True)
